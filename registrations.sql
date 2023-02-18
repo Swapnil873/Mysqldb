91b8f16ff1c3c749e1a93b8b9c75f5a6bab4f79e
Swapnil873 <55633143+Swapnil873@users.noreply.github.com>
2023-02-18 14:00:40 +0530

    Create registrations.sql

---
+++ registrations.sql
@@ -0,0 +1,55 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "CREATE DATABASE Register";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$sql = "CREATE TABLE Resgistrations (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Eventname VARCHAR(30) NOT NULL,
+Name VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL,
+phno VARCHAR(10) NOT NULL,
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+if (mysqli_query($conn, $sql)) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . mysqli_error($conn);
+}
+
+if(isset($_POST['']))
+{	 
+	 $Eventname = $_POST['Eventname'];
+	 $Name = $_POST['Name'];
+	 $email = $_POST['email'];
+	 $phno = $_POST['phno'];
+	 $sql = "INSERT INTO Registrations (Eventname,Name,email,phno,reg_date)
+	 VALUES ('$Eventname','$Name','$email','$phno','$reg_date')";
+	 if (mysqli_query($conn, $sql)) {
+		echo "New record created successfully !";
+	 } else {
+		echo "Error: " . $sql . "
+		" . mysqli_error($conn);
+	 }
+	 
+$sql = "DELETE FROM Registrations WHERE id='" . $_GET[""] . "'";
+if (mysqli_query($conn, $sql)) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . mysqli_error($conn);
+}
+$conn->close();
+?>
